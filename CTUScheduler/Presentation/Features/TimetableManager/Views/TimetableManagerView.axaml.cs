80297a4590b3cafc34016c2ef95a8cc06e698ca0
d3nhatv0lam <d3nhatv0lam@gmail.com>
2025-09-18 20:45:43 +0700

    bugfix: Sync file location in local repo

---
+++ CTUScheduler/Presentation/Features/TimetableManager/Views/TimetableManagerView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.ReactiveUI;
+using CTUScheduler.Presentation.Features.TimetableManager.ViewModels;
+using ReactiveUI;
+
+namespace CTUScheduler.Presentation.Features.TimetableManager.Views;
+
+public partial class TimetableManagerView : ReactiveUserControl<TimetableManagerViewModel>
+{
+    public TimetableManagerView()
+    {
+        InitializeComponent();
+    }
+}
