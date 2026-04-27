9190fb50ab88f8e7fa5e314be10e452ec24e588d
d3nhatv0lam <d3nhatv0lam@gmail.com>
2026-04-27 15:24:44 +0700

    feat: button ripple style

--- CTUScheduler/Presentation/Shared/Styles/Assists/ShadowAssist.cs
+++ CTUScheduler/Presentation/Shared/Styles/Assists/ShadowAssist.cs
@@ -84,16 +84,16 @@ public static class ShadowAssist
 
     private static void ShadowDepthChangedCallback(AvaloniaPropertyChangedEventArgs args)
     {
-        // LƯU Ý: Code này chỉ hoạt động trên Border
         if (args.Sender is Border border)
         {
-            var depth = args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0;
-            var newShadow = depth.ToBoxShadows();
-            // border.BoxShadow =
-            //     (args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0)
-            //     .ToBoxShadows();
+            border.BoxShadow =
+                (args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0)
+                .ToBoxShadows();
             
-            border.SetCurrentValue(Border.BoxShadowProperty, newShadow);
+            // var depth = args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0;
+            // var newShadow = depth.ToBoxShadows();
+            //
+            // border.SetValue(Border.BoxShadowProperty, newShadow);
         }
     }
 
