d2f82af2331ee2161751cc685fc6062d5f4495e9
d3nhatv0lam <d3nhatv0lam@gmail.com>
2026-05-02 22:29:26 +0700

    chores: clean some styles, controls

--- CTUScheduler/Presentation/Features/Home/Views/HomeView.axaml.cs
+++ CTUScheduler/Presentation/Features/Home/Views/HomeView.axaml.cs
@@ -18,17 +18,15 @@ public partial class HomeView : ReactiveUserControl<HomeViewModel>
 
         this.WhenActivated(disposables =>
         {
-            this.OneWayBind(ViewModel, vm => vm.RegistrationInfo, v => v.txtRegistrationInfo.Text, info => info == null ? 
-                            "Không lấy được thông tin!" : 
-                            $"Học kỳ: {info.Semester ?? "?"}\n{info.AcademicYear}-{info.AcademicYear+1}\nSố tín chỉ tối đa: {info.MaxCreditPerSemester}").DisposeWith(disposables);
+            this.OneWayBind(ViewModel, vm => vm.RegistrationInfo, v => v.txtRegistrationInfo.Text,
+                    info => info == null
+                        ? "Không lấy được thông tin!"
+                        : $"Học kỳ: {info.Semester ?? "?"}\n{info.AcademicYear}-{info.AcademicYear + 1}\nSố tín chỉ tối đa: {info.MaxCreditPerSemester}")
+                .DisposeWith(disposables);
 
-            this.OneWayBind(ViewModel, vm => vm.RegistrationInfo, v => v.txtRegistrationTime.Text, info => info == null ?
-                            "Không lấy được thông tin!" :
-                            info.Period).DisposeWith(disposables);
-            
-            this.OneWayBind<HomeViewModel, HomeView, List<PeriodItem>, IEnumerable>(ViewModel, vm => vm.RegistrationInfo.UserPeriod, v => v.dtgridUserRegistrationTime.ItemsSource, userRegistrationTime => userRegistrationTime ?? Enumerable.Empty<PeriodItem>()).DisposeWith(disposables);
-
-            this.OneWayBind<HomeViewModel, HomeView, List<GroupItem>, IEnumerable>(ViewModel, vm => vm.RegistrationInfo.Groups, v => v.dtgridGroupList.ItemsSource, groups => groups ?? Enumerable.Empty<GroupItem>()).DisposeWith(disposables);
+            this.OneWayBind<HomeViewModel, HomeView, List<GroupItem>, IEnumerable>(ViewModel,
+                vm => vm.RegistrationInfo.Groups, v => v.dtgridGroupList.ItemsSource,
+                groups => groups ?? Enumerable.Empty<GroupItem>()).DisposeWith(disposables);
         });
     }
 }
