565eade444817ed33c2257eeb9b1adeb5319e542
d3nhatv0lam <d3nhatv0lam@gmail.com>
2026-05-12 23:04:58 +0700

    bugfix: rename to FindCourseView

--- CTUScheduler/Presentation/Features/Scheduling/Views/Steps/FindCourseView.axaml.cs
+++ CTUScheduler/Presentation/Features/Scheduling/Views/Steps/FindCourseView.axaml.cs
@@ -1,16 +1,13 @@
-using System.Reactive.Disposables;
 using System.Reactive.Disposables.Fluent;
-using Avalonia;
-using Avalonia.Input;
 using ReactiveUI.Avalonia;
 using CTUScheduler.Presentation.Features.Scheduling.ViewModels.Steps;
 using ReactiveUI;
 
 namespace CTUScheduler.Presentation.Features.Scheduling.Views.Steps;
 
-public partial class HandmadeFindCourseView : ReactiveUserControl<FindCourseViewModel>
+public partial class FindCourseView : ReactiveUserControl<FindCourseViewModel>
 {
-    public HandmadeFindCourseView()
+    public FindCourseView()
     {
         InitializeComponent();
         this.WhenActivated(disposables =>
