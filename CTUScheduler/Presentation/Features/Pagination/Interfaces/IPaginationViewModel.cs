8b00dabda0d98cece7579609395329614ff4d359
Duong Minh Duc <110104167+d3nhatv0lam@users.noreply.github.com>
2026-06-21 11:30:35 +0700

    refactor: update pagination AutoRefresh options and improve performance

--- CTUScheduler/Presentation/Features/Pagination/Interfaces/IPaginationViewModel.cs
+++ CTUScheduler/Presentation/Features/Pagination/Interfaces/IPaginationViewModel.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using CTUScheduler.Core.Interfaces;
 using DynamicData;
 
 namespace CTUScheduler.Presentation.Features.Pagination.Interfaces;
 
-public interface IPaginationViewModel<T>: IPaginationInteraction where T: class
+public interface IPaginationViewModel<T> : IPaginationInteraction where T : class, INotifyPropertyChanged
 {
     /// <summary>
     /// Data that has been added in the current time, not reactive.
     /// </summary>
     public IEnumerable<T> CurrentData { get; }
+
     /// <summary>
     /// Reactive data that has been added and paged.
     /// </summary>
     public ReadOnlyObservableCollection<T> PagedData { get; }
+
     public void Add(T item);
     public void AddRange(IEnumerable<T> items);
     public void Clear();
