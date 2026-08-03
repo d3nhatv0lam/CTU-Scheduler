8d5d46fcefbc5c8a5105c1be63f0de0ad530b768
Duong Minh Duc <110104167+d3nhatv0lam@users.noreply.github.com>
2026-08-03 18:36:04 +0700

    chore: add warning disable IL2091 in LazyService

--- CTUScheduler/AppServices/Models/LazyService.cs
+++ CTUScheduler/AppServices/Models/LazyService.cs
@@ -3,7 +3,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CTUScheduler.AppServices.Models;
 
-public class LazyService<T>: Lazy<T> where T : class
+#pragma warning disable IL2091
+public class LazyService<T> : Lazy<T> where T : class
 {
-    public LazyService(IServiceProvider provider): base(provider.GetRequiredService<T>) {}
+    public LazyService(IServiceProvider provider) : base(provider.GetRequiredService<T>)
+    {
+    }
 }
+#pragma warning restore IL2091
