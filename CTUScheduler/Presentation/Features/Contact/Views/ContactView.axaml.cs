072a3bfb1a09fec4abc5eeaa8bf9d037bf178abf
Duong Minh Duc <110104167+d3nhatv0lam@users.noreply.github.com>
2026-08-18 21:05:18 +0700

    feat: add darkmode & fix: hover animation in social button

--- CTUScheduler/Presentation/Features/Contact/Views/ContactView.axaml.cs
+++ CTUScheduler/Presentation/Features/Contact/Views/ContactView.axaml.cs
@@ -1,6 +1,4 @@
-using Avalonia.Markup.Xaml;
 using ReactiveUI.Avalonia;
-using ReactiveUI;
 using CTUScheduler.Presentation.Features.Contact.ViewModels;
 
 namespace CTUScheduler.Presentation.Features.Contact.Views;
@@ -10,10 +8,5 @@ public partial class ContactView : ReactiveUserControl<ContactViewModel>
     public ContactView()
     {
         InitializeComponent();
-    }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
     }
 }
