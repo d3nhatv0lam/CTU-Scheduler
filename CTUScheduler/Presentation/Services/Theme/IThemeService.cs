a2955e55c5111e1651853fa8a7e627fb8da7a394
Duong Minh Duc <110104167+d3nhatv0lam@users.noreply.github.com>
2026-08-21 09:48:01 +0700

    feat: ThemeService

---
+++ CTUScheduler/Presentation/Services/Theme/IThemeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reactive;
+
+namespace CTUScheduler.Presentation.Services.Theme;
+
+/// <summary>
+/// Notify when App theme changed
+/// </summary>
+public interface IThemeService
+{
+    IObservable<Unit> ThemeChanged { get; } 
+}
---
+++ CTUScheduler/Presentation/Services/Theme/ThemeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Avalonia;
+
+namespace CTUScheduler.Presentation.Services.Theme;
+
+
+public class ThemeService: IThemeService
+{
+    public ThemeService()
+    {
+        if (Application.Current is null)
+        {
+            throw new InvalidOperationException("Application.Current is null.");
+        }
+
+        ThemeChanged = Observable.FromEvent<EventHandler, EventArgs>(
+                h => (s, e) => h(e),
+                h => Application.Current.ActualThemeVariantChanged += h,
+                h => Application.Current.ActualThemeVariantChanged -= h)
+            .Select(_ => Unit.Default)
+            .Publish()
+            .RefCount();
+    }
+    
+    public IObservable<Unit> ThemeChanged { get; }
+}
